68c2987f55c5dc9d6f20c79e63689078d099b2fa
sirchras <cjlynch1710@gmail.com>
2024-11-15 18:57:08 +1300

    rewrite class as module
    - added more vector functions
    - added arithmetic metamethods
    - added tostring metamethod
    - added type checks to most functions
    - added some scope limits

--- vector.lua
+++ vector.lua
@@ -1,47 +1,114 @@
-sqrt=math.sqrt
+local sqrt = math.sqrt
 
-class={}
-function class:new(o)
-	local o=o or {}
-	setmetatable(o,self)
-	self.__index=self
-	return o
+local vector = {}
+vector.__index = vector
+
+local function isvector(t)
+	return getmetatable(t) == vector
 end
 
-_vector=class:new{
-	x=0,
-	y=0,
+local function new(x, y)
+	local o = {x = x or 0, y = y or 0}
+	assert(type(o.x) == "number" and type(o.y) == "number",
+	 "wrong argument types: expected <number> and <number>")
+	return setmetatable(o, vector)
+end
+
+function vector:clone()
+	return new(self.x, self.y)
+end
+
+function vector.iseq(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	return a.x == b.x and a.y == b.y
+end
+vector.__eq = vector.iseq
+
+function vector.__unm(a)
+	assert(isvector(a), "wrong argument type: expected <vector>")
+	return new(-a.x, -a.y)
+end
+
+function vector.add(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	return new(a.x + b.x, a.y + b.y)
+end
+vector.__add = vector.add
+
+function vector.sub(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	return new(a.x - b.x, a.y - b.y)
+end
+vector.__sub = vector.sub
+
+function vector.dot(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	return (a.x * b.x) + (a.y * b.y)
+end
+
+function vector.mult(a ,b)
+	if type(b) == "number" then
+		return new(a.x * b, a.y * b)
+	elseif type(a) == "number" then
+		return new(b.x * a, b.y * a)
+	else
+		assert(isvector(a) and isvector(b),
+		 "wrong argument types: expected <vector> or <number>")
+		return a:dot(b)
+	end
+end
+vector.__mul = vector.mult
+
+function vector.div(a, b)
+	assert(isvector(a) and type(b) == "number",
+	 "wrong argument types: expected <vector> and <number>")
+	assert(b ~= 0, "invalid argument: <number> should be non-zero")
+	return new(a.x / b, a.y / b)
+end
+vector.__div = vector.div
+
+function vector.magsq(a)
+	assert(isvector(a), "wrong argument type: expected <vector>")
+	return a:dot(a)
+end
+
+function vector.mag(a)
+	assert(isvector(a), "wrong argument type: expected <vector>")
+	return sqrt(a:magsq())
+end
+vector.__len = vector.mag
+
+function vector.norm(a)
+	assert(isvector(a), "wrong argument type: expected <vector>")
+	local mag = a:mag()
+	if mag <= 1 then
+		return a:clone()
+	else
+		return new(a.x / mag, a.y / mag)
+	end
+end
+
+function vector:__tostring()
+	return "(" .. self.x .. "," .. self.y .. ")"
+end
+
+local module = {
+	new = new,
+	add = vector.add,
+	sub = vector.sub,
+	mult = vector.mult,
+	div = vector.div,
+	dot = vector.dot,
+	magsq = vector.magsq,
+	mag = vector.mag,
+	norm = vector.norm,
+	clone = vector.clone,
+	isvector = isvector
 }
-function _vector.add(self,v)
-	return _vector:new{
-		x=self.x+v.x,
-		y=self.y+v.y
-	}
-end
-function _vector.sub(self,v)
-	return _vector:new{
-		x=self.x-v.x,
-		y=self.y-v.y
-	}
-end
-function _vector.mult(self,s)
-	assert(type(s)=="number")
-	return _vector:new{
-		x=self.x*s,
-		y=self.y*s
-	}
-end
-function _vector.div(self,s)
-	assert(type(s)=="number")
-	assert(s!=0)
-	return _vector:new{
-		x=self.x/s,
-		y=self.y/s
-	}
-end
-function _vector.magsq(self)
-	return (self.x*self.x)+(self.y*self.y)
-end
-function _vector.mag(self)
-	return sqrt(self:magsq())
-end
+return setmetatable(module, {
+	__call = function(_,...) return new(...) end
+})
