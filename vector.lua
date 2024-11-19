c092ee4e911c99cf655adba599d1e51c68786408
sirchras <cjlynch1710@gmail.com>
2024-11-19 19:57:24 +1300

    add new vector functions, comments
    - adds random, fromangle, angle, and dist functions
    - adds comments

--- vector.lua
+++ vector.lua
@@ -1,12 +1,15 @@
-local sqrt = math.sqrt
+local PI = math.pi
 
+-- vector class
 local vector = {}
 vector.__index = vector
 
+-- check if obj is a vector
 local function isvector(t)
 	return getmetatable(t) == vector
 end
 
+-- create new vector
 local function new(x, y)
 	local o = {x = x or 0, y = y or 0}
 	assert(type(o.x) == "number" and type(o.y) == "number",
@@ -14,10 +17,24 @@ local function new(x, y)
 	return setmetatable(o, vector)
 end
 
+-- create new unit vector from angle (in tau)
+local function fromangle(angle)
+	-- angle in PICO-8 is expressed in fractions of 2 * PI
+	local angle = angle * 2 * PI
+	return new(math.cos(angle), -math.sin(angle))
+end
+
+-- create a new random unit vector
+local function random()
+	return fromangle(math.random())
+end
+
+-- return a clone of the vector
 function vector:clone()
 	return new(self.x, self.y)
 end
 
+-- check if two vectors are equal
 function vector.iseq(a, b)
 	assert(isvector(a) and isvector(b),
 	 "wrong argument types: expected <vector> and <vector>")
@@ -25,11 +42,13 @@ function vector.iseq(a, b)
 end
 vector.__eq = vector.iseq
 
+-- negate a vector (x,y) -> (-x,-y)
 function vector.__unm(a)
 	assert(isvector(a), "wrong argument type: expected <vector>")
 	return new(-a.x, -a.y)
 end
 
+-- return the sum of two vectors
 function vector.add(a, b)
 	assert(isvector(a) and isvector(b),
 	 "wrong argument types: expected <vector> and <vector>")
@@ -37,6 +56,7 @@ function vector.add(a, b)
 end
 vector.__add = vector.add
 
+-- return the difference of two vectors
 function vector.sub(a, b)
 	assert(isvector(a) and isvector(b),
 	 "wrong argument types: expected <vector> and <vector>")
@@ -44,13 +64,15 @@ function vector.sub(a, b)
 end
 vector.__sub = vector.sub
 
+-- return the dot product of two vectors
 function vector.dot(a, b)
 	assert(isvector(a) and isvector(b),
 	 "wrong argument types: expected <vector> and <vector>")
 	return (a.x * b.x) + (a.y * b.y)
 end
 
-function vector.mult(a ,b)
+-- return the product of scalar/vector or vector/vector multiplication
+function vector.mult(a, b)
 	if type(b) == "number" then
 		return new(a.x * b, a.y * b)
 	elseif type(a) == "number" then
@@ -63,6 +85,7 @@ function vector.mult(a ,b)
 end
 vector.__mul = vector.mult
 
+-- return the result of dividing the vector by non-zero scalar
 function vector.div(a, b)
 	assert(isvector(a) and type(b) == "number",
 	 "wrong argument types: expected <vector> and <number>")
@@ -71,17 +94,20 @@ function vector.div(a, b)
 end
 vector.__div = vector.div
 
+-- return the squared magnitude of the vector
 function vector.magsq(a)
 	assert(isvector(a), "wrong argument type: expected <vector>")
 	return a:dot(a)
 end
 
+-- return the magnitude of the vector
 function vector.mag(a)
 	assert(isvector(a), "wrong argument type: expected <vector>")
-	return sqrt(a:magsq())
+	return math.sqrt(a:magsq())
 end
 vector.__len = vector.mag
 
+-- return a norm of the vector
 function vector.norm(a)
 	assert(isvector(a), "wrong argument type: expected <vector>")
 	local mag = a:mag()
@@ -92,12 +118,49 @@ function vector.norm(a)
 	end
 end
 
+-- return the angle heading of the vector (in tau)
+function vector.heading(a)
+	assert(isvector(a), "wrong argument type: expected <vector>")
+	-- this is what works for some reason
+	-- really thought I'd have to invert the y arg to match PICO-8
+	return math.atan(a.y, a.x) / (2 * PI) % 1
+end
+
+-- return the distance between the vectors
+function vector.dist(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	local diff = vector.sub(a, b)
+	return diff:mag()
+end
+
+-- todo: better function name?
+-- return the angle between the vectors
+function vector.angle(a, b)
+	assert(isvector(a) and isvector(b),
+	 "wrong argument types: expected <vector> and <vector>")
+	local a, b = a:norm(), b:norm()
+	if a:iseq(b) then
+		return 0.0
+	end
+	-- floating point / nan headaches ;-;
+	return math.acos(a:dot(b)) / (2 * PI)
+end
+-- todo:
+-- could use math.modf to try handle small fractional differences
+-- could also just math.abs the difference in vector headings
+
+-- return string representation of the vector
 function vector:__tostring()
 	return "(" .. self.x .. "," .. self.y .. ")"
 end
 
+-- create the module
 local module = {
 	new = new,
+	isvector = isvector,
+	random = random,
+	fromangle = fromangle,
 	add = vector.add,
 	sub = vector.sub,
 	mult = vector.mult,
@@ -107,7 +170,9 @@ local module = {
 	mag = vector.mag,
 	norm = vector.norm,
 	clone = vector.clone,
-	isvector = isvector
+	heading = vector.heading,
+	dist = vector.dist,
+	angle = vector.angle,
 }
 return setmetatable(module, {
 	__call = function(_,...) return new(...) end
